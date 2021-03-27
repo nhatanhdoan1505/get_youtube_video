1ee6a8edf9569504b984aa71aace8c6f9ab5ed4a
Doan Nhat Anh <nhatanhdoan1505@gmail.com>
2021-03-27 22:08:26 +0700

    fix

--- app.js
+++ app.js
@@ -95,5 +95,5 @@ app.post("/api/keyword", (request, respose) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
